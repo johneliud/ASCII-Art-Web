feba40e2ab7b4c067ac395e92ad711b141d7aa81
John <johneliud4@gmail.com>
2024-07-12 15:32:10 +0300

    Chore: Ensure the number of arguments passed is correct

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/johneliud/ASCII-Art-Web/serverhandlers"
+)
+
+func main() {
+	if len(os.Args) != 1 {
+		fmt.Println("Usage: go run .")
+		return
+	}
+	
+	serverhandlers.StartServer()
+}
