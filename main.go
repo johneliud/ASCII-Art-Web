f45f76768f42dfa31e970f346c40119210b9e3b8
John <johneliud4@gmail.com>
2024-07-24 21:19:12 +0300

    Add: 1. Define http.Handle() function that serves files present in the static directory. 2. Include clause to have the user define their desired port to run the program from the CLI arguments. 3. Check if the port passed by the user is within the valid range of 1024-65535.

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/johneliud/ASCII-Art-Web/serverhandlers"
 )
@@ -12,5 +15,23 @@ func main() {
 		fmt.Println("Usage: go run . [PORT]")
 		return
 	}
-	serverhandlers.StartServer()
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", serverhandlers.HomeHandler)
+	http.HandleFunc("/ascii-art", serverhandlers.AsciiArtHandler)
+
+	port, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("Cannot convert %v to integer: %v\n", os.Args[1], err)
+		return
+	}
+
+	// Check if portNum is within a valid range
+	if port < 1024 || port > 65535 {
+		fmt.Println("Port to running the server should be within the range 1024 to 65535")
+		return
+	}
+	listeningPort := ":" + strconv.Itoa(port)
+	log.Printf("Server running on http://localhost%v\n", listeningPort)
+	http.ListenAndServe(listeningPort, nil)
 }
