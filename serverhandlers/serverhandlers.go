4380deefdc3089155ecc6e9ee0ea778312cccd8a
John <johneliud4@gmail.com>
2024-07-24 21:26:03 +0300

    Update: Remove StartServer() function from file and have its functionality checked in the main function.

--- serverhandlers/serverhandlers.go
+++ serverhandlers/serverhandlers.go
@@ -3,10 +3,7 @@ package serverhandlers
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
-	"os"
-	"strconv"
 	"strings"
 
 	"github.com/johneliud/ASCII-Art-Web/printart"
@@ -17,9 +14,7 @@ type Page struct {
 	AsciiArt, Input string
 }
 
-/*
-Function HomeHandler is an HTTP handler function that serves the home page of the ASCII Art Web application. If the root path is requested, it attempts to parse and execute the "templates/index.html" template file.
-*/
+// Function HomeHandler is an HTTP handler function that serves the home page of the ASCII Art Web application. If the root path is requested, it attempts to parse and execute the "templates/index.html" template file.
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
@@ -34,9 +29,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-/*
-Function AsciiArtHandler is a HTTP handler function that processes ASCII art requests. It accepts POST requests at the "/ascii-art" path and generates ASCII art based on the provided input string and banner.
-*/
+// Function AsciiArtHandler is a HTTP handler function that processes ASCII art requests. It accepts POST requests at the "/ascii-art" path and generates ASCII art based on the provided input string and banner.
 func AsciiArtHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, " 405 method not allowed", http.StatusMethodNotAllowed)
@@ -46,12 +39,12 @@ func AsciiArtHandler(w http.ResponseWriter, r *http.Request) {
 	// Retrieves the input string and banner file from the request form data
 	inputString := r.FormValue("input")
 	bannerFile := r.FormValue("banner")
-	
+
 	if inputString == "" {
 		http.Error(w, "400 bad request", http.StatusBadRequest)
 		return
 	}
-	
+
 	// Sets the default banner selection to be standard if not specified/selected
 	if bannerFile == "" {
 		bannerFile = "standard"
@@ -82,28 +75,4 @@ func AsciiArtHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	tmpl.Execute(w, data)
-}
-
-/*
-Function StartServer sets up and starts the HTTP server for the ASCII Art Web application. It serves static files from the "static" directory, and registers handlers for the root ("/") and "/ascii-art" paths.
-*/
-func StartServer() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", HomeHandler)
-	http.HandleFunc("/ascii-art", AsciiArtHandler)
-	
-	port, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Printf("Cannot convert %v to integer: %v\n", os.Args[1], err)
-		return
-	}
-
-	// Check if portNum is within a valid range
-	if port < 1024 || port > 65535 {
-		fmt.Println("Port to running the server should be within the range 1024 to 65535")
-		return
-	}
-	listeningPort := ":" + strconv.Itoa(port)
-	log.Printf("Server running on http://localhost%v\n", listeningPort)
-	http.ListenAndServe(listeningPort, nil)
 }
