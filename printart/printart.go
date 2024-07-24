249cc3e611ec093df22f01efc09aae63db9fad2a
John <johneliud4@gmail.com>
2024-07-24 21:21:58 +0300

    Update: Update function PrintArt() by commenting out various unneccessary parts of the code.

--- printart/printart.go
+++ printart/printart.go
@@ -5,19 +5,21 @@ import (
 	"strings"
 )
 
-/*
-PrintArt takes a slice of strings representing ASCII art, an input string, and a strings.Builder. It prints the input string as ASCII art using the provided ASCII art slice. The ASCII art is constructed by replacing certain characters in the input string with their corresponding ASCII representations. If the input string contains unprintable sequences or characters not present in the ASCII manual, an error message is printed and the program exits.
-*/
+// PrintArt takes a slice of strings representing ASCII art, an input string, and a strings.Builder. It prints the input string as ASCII art using the provided ASCII art banner. The ASCII art is constructed by replacing certain characters in the input string with their corresponding ASCII representations.
 func PrintArt(bannerFileSlice []string, inputString string, output *strings.Builder) error {
 	switch inputString {
-	case "\\n":
-		output.WriteString("\n")
-		return nil
+	/*
+		case "\\n":
+			output.WriteString("\n")
+			return nil
+	*/
 	case "":
 		return nil
-	case "\\t":
-		output.WriteString("    ")
-		return nil
+		/*
+			case "\\t":
+				output.WriteString("    ")
+				return nil
+		*/
 	}
 
 	// Handle unprintable sequences
@@ -27,9 +29,11 @@ func PrintArt(bannerFileSlice []string, inputString string, output *strings.Buil
 			return fmt.Errorf("input string contains an unprintable sequence")
 		}
 	}
-	tabCharacterText := strings.ReplaceAll(inputString, "\\t", "    ")
-	newlineCharacterText := strings.ReplaceAll(tabCharacterText, "\\n", "\n")
-	splitArguments := strings.Split(newlineCharacterText, "\n")
+	/*
+		tabCharacterText := strings.ReplaceAll(inputString, "\\t", "    ")
+		newlineCharacterText := strings.ReplaceAll(tabCharacterText, "\\n", "\n")
+	*/
+	splitArguments := strings.Split(inputString, "\r\n")
 
 	// Handle characters not present in the ASCII manual
 	for _, argument := range splitArguments {
