306497d47ff5ed2b6f4183cfa942bb03ab6a51d5
John <johneliud4@gmail.com>
2024-07-28 17:07:24 +0300

    Update: Update main function to have the web server run on a default port if not specified.

--- main.go
+++ main.go
@@ -11,8 +11,30 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run . [PORT]")
+	var (
+		port          int
+		listeningPort string
+	)
+
+	switch len(os.Args) {
+	case 1:
+		port = 8080
+		listeningPort = ":" + strconv.Itoa(port)
+	case 2:
+		port, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Printf("Cannot convert %v to integer: %v\n", os.Args[1], err)
+			return
+		}
+
+		// Check if portNum is within a valid range
+		if port < 1024 || port > 65535 {
+			fmt.Println("Port to running the server should be within the range 1024 to 65535")
+			return
+		}
+		listeningPort = ":" + strconv.Itoa(port)
+	default:
+		fmt.Println("Incorrect number of arguments passed.")
 		return
 	}
 
@@ -20,18 +42,6 @@ func main() {
 	http.HandleFunc("/", serverhandlers.HomeHandler)
 	http.HandleFunc("/ascii-art", serverhandlers.AsciiArtHandler)
 
-	port, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Printf("Cannot convert %v to integer: %v\n", os.Args[1], err)
-		return
-	}
-
-	// Check if portNum is within a valid range
-	if port < 1024 || port > 65535 {
-		fmt.Println("Port to running the server should be within the range 1024 to 65535")
-		return
-	}
-	listeningPort := ":" + strconv.Itoa(port)
 	log.Printf("Server running on http://localhost%v\n", listeningPort)
 	http.ListenAndServe(listeningPort, nil)
 }
