83ebf41780c79bbdce7f05bb9c51c6cf58659362
John <johneliud4@gmail.com>
2024-08-04 10:14:21 +0300

    Fix: Modify HTTP error codes to display HTML files instead of writing text on the web page.

--- serverhandlers/serverhandlers.go
+++ serverhandlers/serverhandlers.go
@@ -14,38 +14,36 @@ type Page struct {
 	AsciiArt, Input string
 }
 
-// Function HomeHandler is an HTTP handler function that serves the home page of the ASCII Art Web application. If the root path is requested, it attempts to parse and execute the "templates/index.html" template file.
+// Function HomeHandler serves the home page of the ASCII Art Web application.
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		serve404Page(w, r)
 		return
 	}
 
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
-		http.Error(w, "an unexpected error occurred. please try again later", http.StatusInternalServerError)
+		serve404Page(w, r)
 		return
 	}
 	tmpl.Execute(w, nil)
 }
 
-// Function AsciiArtHandler is a HTTP handler function that processes ASCII art requests. It accepts POST requests at the "/ascii-art" path and generates ASCII art based on the provided input string and banner.
+// Function AsciiArtHandler accepts POST requests at the "/ascii-art" path and generates ASCII art based on the provided input string and banner.
 func AsciiArtHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Error(w, " 405 method not allowed", http.StatusMethodNotAllowed)
+		serve405Page(w, r)
 		return
 	}
 
-	// Retrieves the input string and banner file from the request form data
 	inputString := r.FormValue("input")
 	bannerFile := r.FormValue("banner")
 
 	if inputString == "" {
-		http.Error(w, "400 bad request", http.StatusBadRequest)
+		serve400Page(w, r)
 		return
 	}
 
-	// Sets the default banner selection to be standard if not specified/selected
 	if bannerFile == "" {
 		bannerFile = "standard"
 	}
@@ -53,14 +51,14 @@ func AsciiArtHandler(w http.ResponseWriter, r *http.Request) {
 	bannerFilePath := fmt.Sprintf("banners/%s.txt", bannerFile)
 	bannerFileSlice, err := readandprocess.ReadAndProcess(bannerFilePath)
 	if err != nil {
-		http.Error(w, "an unexpected error occurred. please try again later", http.StatusInternalServerError)
+		serve500Page(w, r)
 		return
 	}
 
 	var output strings.Builder
 	err = printart.PrintArt(bannerFileSlice, inputString, &output)
 	if err != nil {
-		http.Error(w, "an unexpected error occurred. please try again later", http.StatusInternalServerError)
+		serve400Page(w, r)
 		return
 	}
 
@@ -71,7 +69,7 @@ func AsciiArtHandler(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
-		http.Error(w, "an unexpected error occurred. please try again later", http.StatusInternalServerError)
+		serve404Page(w, r)
 		return
 	}
 	tmpl.Execute(w, data)
